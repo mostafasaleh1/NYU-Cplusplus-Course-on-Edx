72e69f7364a2087c1d4095ba96ef53c85f229943
Mostafa Saleh <mostafasaleh2055@gmail.com>
2021-08-16 21:28:43 +0200

    Third programme, week 2, lecture 3 --- 2;

--- NYU C++ Course on Edx.cpp
+++ NYU C++ Course on Edx.cpp
@@ -1,16 +1,34 @@
-#define _USE_MATH_DEFINES
-
 #include <iostream>
-#include <cmath>
 
 using namespace std;
+
 int main()
 {
-	//This programme takes an input character from the user and converts it into the corresponding ASCII number.
-	char x;
-	cout << "Please enter a character: " << endl;
-	cin >> x;
-	//Casting the character into integer value, and printing the result.
-	cout << "the corresponding ASCII number is:  " << (int)x << endl;
+	
+	//This programme takes an upper case letter from the user and converts it to a lower case letter and vice versa.
+	char x, zzz;
+	do
+	{
+		cout << "please enter a character" << endl;
+		cin >> x;
+
+		if (x >= 65 && x <= 90)
+		{
+			cout << (char)(x + 32) << endl;
+		}
+		else if (x >= 97 && x <= 122)
+		{
+			cout << (char)(x - 32) << endl;
+		}
+		else {
+			cout << "incorrect input" << endl;
+		}
+
+		cout << "Do you want to do it again? y/n  :   ";
+		cin >> zzz;
+	} while (zzz == 'y');
+
+	cout << "See you" << endl;
+
 	return 0;
 }
