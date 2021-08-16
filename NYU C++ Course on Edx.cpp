efa2de8f5e9e15f18dac444382e962b4dde4ca2e
Mostafa Saleh <mostafasaleh2055@gmail.com>
2021-08-16 07:36:49 +0200

    the second progeamme, week 2, lecture 2

--- NYU C++ Course on Edx.cpp
+++ NYU C++ Course on Edx.cpp
@@ -1,18 +1,15 @@
+#define _USE_MATH_DEFINES
+
 #include <iostream>
+#include <cmath>
+
 using namespace std;
 int main()
 {
-	int number_of_days_the_user_traveled;
-
-	cout << "please enter the number of days you traveled" << endl;
-
-	cin >> number_of_days_the_user_traveled;
-
-	int number_of_weeks = number_of_days_the_user_traveled / 7;
-
-	int remaining_days = number_of_days_the_user_traveled - (7 * number_of_weeks);
-
-	cout << "(" << number_of_days_the_user_traveled << ") days are (" << number_of_days_the_user_traveled / 7 << ") weeks and (" << remaining_days << ") days" << endl;
+	int radius;
+	cout << "please enter the radius of the circle" << endl;
+	cin >> radius;
+	cout << "The Area of the cicle =   " << M_PI * pow(radius, 2) << endl;
 
 	return 0;
 }
